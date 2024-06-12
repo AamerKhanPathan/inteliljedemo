4fc96da59b9948b6282d654e488310e4c3e7b4eb
AamerKhanPathan <aamerkhanpvt@gmail.com>
2024-06-12 19:19:09 +0530

    add heelo pring msg

--- src/main/java/com/bikkadit/databasedemo/DatabasedemoApplication.java
+++ src/main/java/com/bikkadit/databasedemo/DatabasedemoApplication.java
@@ -1,5 +1,6 @@
 package com.bikkadit.databasedemo;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,8 @@ public class DatabasedemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DatabasedemoApplication.class, args);
+
+		System.out.println("hello");
 	}
 
 }
